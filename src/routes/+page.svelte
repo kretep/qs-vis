6d97b518779c33390ec379c55c06391e6a6214e9
Peter Kok <kretep@users.noreply.github.com>
2023-09-19 15:45:16 +0200

    Vega spec in separate file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from 'svelte';
   import { dataStore } from './dataStore.js';
+  import spec1 from './spec1.json';
 
   let container;
 
@@ -38,58 +39,8 @@
 
         console.log(jsonArray);
 
-        const chartSpec = {
-          "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-          "width": 800,
-          "height": 600,
-          "padding": 5,
-          "layer": [{
-            "data": {
-              "name": "table",
-              "values": jsonArray
-            },
-            "encoding": {
-              "x": {
-                "field": "date",
-                "type": "temporal",
-                "axis": {"format": "%Y-%m-%d", "labelAngle": 45}
-              },
-              "y": {
-                "field": "speed",
-                "type": "quantitative",
-                "axis": {
-                  "labelExpr": "datum.value === null ? null : timeFormat(datum.value * 60 * 1000, '%M:%S', 'utc')",
-                  "tickMinStep": {"seconds": 1},
-                  "scale": {"zero": false}
-                }
-              }
-            },
-            "mark": {
-              "type": "point",
-              "tooltip": {"content": "data"}
-            }
-          }, {
-            "data": {
-              "values": [
-                {"year": "2015-01-01"},
-                {"year": "2016-01-01"},
-                {"year": "2017-01-01"},
-                {"year": "2018-01-01"},
-                {"year": "2019-01-01"},
-                {"year": "2020-01-01"},
-                {"year": "2021-01-01"},
-                {"year": "2022-01-01"},
-                {"year": "2023-01-01"},
-                {"year": "2024-01-01"}
-              ]
-            },
-            "mark": {"type": "rule", "color": "black", "size": 2},
-            "encoding": {
-              "x": {"field": "year", "type": "temporal", "axis": {}},
-              "y": {"value": 0}
-            }
-          }]
-        };
+        const chartSpec = spec1;
+        spec1.layer[0].data.values = jsonArray;
         vegaEmbed(container, chartSpec, { actions: false });
       }
     });
